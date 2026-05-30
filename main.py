9b578f5bd90194890f74b3ebe4f56cf6f83056f1
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 04:11:25 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterFenderWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterFenderWorker()
+    print("Worker engine initialized.")
